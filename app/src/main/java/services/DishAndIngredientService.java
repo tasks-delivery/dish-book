ac72430f29c4d2c93016cf5f2601ba322cb8714c
ordeh <ordeh@mail.ru>
2018-09-05 17:55:45 +0300

    remove unused files

--- app/src/main/java/services/DishAndIngredientService.java
+++ app/src/main/java/services/DishAndIngredientService.java
@@ -13,8 +13,6 @@ public class DishAndIngredientService {
 
     private Realm realm;
 
-    private DishService dishService;
-
     private IngredientService ingredientService;
 
     public void deleteIngredientFromDish(String dishName, String ingName){
@@ -94,48 +92,4 @@ public class DishAndIngredientService {
         return freeIngs;
     }
 
-
-    /*
-    private Dish dish;
-
-    private Ingredient ingredient;
-
-    private DatabaseService databaseService = App.getInstance().getDatabaseService();
-
-    private DishDao dishDao = databaseService.dishDao();
-
-    private IngredientDao ingredientDao = databaseService.ingredientDao();
-
-    private List<Ingredient> ingredients;
-
-    private IngredientService ingredientService;
-
-    public void deleteDish(String dish_name){
-        dish = dishDao.findDishByName(dish_name);
-       // ingredients = ingredientDao.getIngredientByDishId((dishDao.findIdByName(dish_name)));
-        ingredientDao.deleteAllIngredients(ingredients);
-        dishDao.delete(dish);
-    }
-
-    public void deleteIngredientFromDish(int dish_id){
-        ingredient = ingredientDao.findIngredientByDishName(dish_id);
-        //ingredient.setDish_id(0);
-        ingredientDao.update(ingredient);
-    }
-
-    public void addIngredientToDish(String dish_name, String ing_name) {
-       dish = dishDao.findDishByName(dish_name);
-       ingredient = ingredientDao.findIngredientByName(ing_name);
-       ingredient.id = dish.getId();
-       ingredientDao.update(ingredient);
-    }
-
-    public List<String> loadAllIngredientsWithoutDish(int dish_id){
-        return ingredientDao.findIngredientsNotAssigneToDishByDishName(dish_id);
-    }
-
-    public List<String> findIngredientsOfDish(int dish_id){
-        return ingredientDao.findIngredientsByDishName(dish_id);
-    }
-    */
 }
--- app/src/main/java/services/IngredientService.java
+++ app/src/main/java/services/IngredientService.java
@@ -74,28 +74,4 @@ public class IngredientService {
         return ingNames;
     }
 
-
-    /*
-    private Ingredient ingredient;
-
-    private DatabaseService databaseService = App.getInstance().getDatabaseService();
-
-    private IngredientDao ingredientDao = databaseService.ingredientDao();
-
-    public List<String> findAllIngredientsNames(){
-        return ingredientDao.findAllIngredientsNames();
-    }
-
-    public void createIngredient(String ing_name) {
-        ingredient = new Ingredient(ing_name);
-     //   ingredient.dishId = 5;
-        ingredientDao.insert(ingredient);
-    }
-
-    public void deleteIngredient(String ing_name){
-        ingredient = ingredientDao.findIngredientByName(ing_name);
-        ingredientDao.delete(ingredient);
-    }
-
-    */
 }
